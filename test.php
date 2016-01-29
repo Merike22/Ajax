fbf411879b4a590e9f2a2381ca980cb63ab3644a
Merike Tomson <merike.tomson@khk.ee>
2016-01-29 13:31:17 +0200

    Lecture 4 AJAX with PHP Using jQuery - Registration Form Project [Delivers #112643313]

--- test.php
+++ test.php
@@ -1,3 +1,24 @@
 <?php
-echo $_GET['name'];
+
+$con = mysqli_connect("localhost", "root", "", "test");
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+$sel = "select * from users where email='$email'";
+
+$run = mysqli_query($con, $sel);
+$check_email = mysqli_num_rows($run);
+if ($check_email==1) {
+    echo"<h2>Selline e-mail on juba olemas, palun sisesta uus!</h2>";
+    exit();
+} else {
+    $insert = "insert into users(name,pass,email) VALUES ('$name','$pass','$email')";
+    $run_insert = mysqli_query($con,$insert);
+    if($run_insert) {
+        echo "<h2>Registreerumine õnnestus! Täname!</h2>";
+    }
+}
+
 ?>
